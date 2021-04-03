9444f05c091c1851d087a650b0b8e1b34797dd31
fujii dan <2607dan@gmail.com>
2021-04-03 19:20:41 +0900

    userモデル作成　github認証

--- config/initializers/ommiauth.rb
+++ config/initializers/ommiauth.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, 
+    provider :github, "3afd5d474d359dc89f0e", "6a797e72fd79b9f0b193846a0b42a38324341627"
   else 
     provider :github,
       Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret] 
+      Rails.application.credentials.github[:client_secret]
+  end    
 end
