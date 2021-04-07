97fe225efa70c25c82c19a62a9dbafb08d6543df
fujii dan <2607dan@gmail.com>
2021-04-07 19:07:12 +0900

    拡張機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :events, only: %i[new create show edit update destroy] do
     resources :tickets, only: %i[new create destroy]
   end
-  resource :retirements, only: %i[ew create]
+  resource :retirements, only: %i[new create]
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
